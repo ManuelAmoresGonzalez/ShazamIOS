d37f0afaf0ce8e4f3f28be3f28dc4bb60b7a8377
Manuel Amores González <56935416+ManuelAmoresGonzalez@users.noreply.github.com>
2022-10-19 10:24:41 -0600

    Add files via upload

---
+++ ShazamIOS/ShazamIOS/ShazamIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShazamIOSApp.swift
+//  ShazamIOS
+//
+//  Created by Manuel Enrique Amores Gonzalez on 19/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ShazamIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
