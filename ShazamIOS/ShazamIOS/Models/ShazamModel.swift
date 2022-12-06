f617d70db945c01ebbee574b460d0906181243ce
Manuel Amores González <amoresgonzalezmanuel@gmail.com>
2022-12-06 13:04:43 -0600

    Creacion interfaz aplicacion

--- ShazamIOS/ShazamIOS/Models/ShazamModel.swift
+++ ShazamIOS/ShazamIOS/Models/ShazamModel.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+struct ShazamModel: Decodable {
+    
+    let title: String?
+    let artist: String?
+    let album: URL?
+}
--- ShazamIOS/ShazamIOS/Views/ShazamView.swift
+++ ShazamIOS/ShazamIOS/Views/ShazamView.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct ShazamView: View {
+    
+    @StateObject private var shazam = ShazamViewModel()
+    
     var body: some View {
-        Text("ShazamView")
+        NavigationView {
+            VStack(alignment: .center){
+                Spacer()
+                if shazam.recording {
+                    ProgressView()
+                }
+                AsyncImage(url: shazam.shazamModel.album){ image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .edgesIgnoringSafeArea(.all)
+                } placeholder: {
+                    EmptyView()
+                }
+                
+                Text(shazam.shazamModel.title ?? "Sin titulo")
+                    .font(.title)
+                    .bold()
+                
+                Text(shazam.shazamModel.artist ?? "Sin artista")
+                    .font(.title2)
+                    .bold()
+                
+                Spacer()
+                HStack{
+                    Button( action: {
+                        shazam.startListening()
+                    }){
+                        Text( shazam.recording ? "Escuchando" : "Escuchar")
+                    }
+                    .buttonStyle(.bordered)
+                    .controlSize(.large)
+                    .shadow(radius: 4)
+                    .tint(.orange)
+                }
+                Spacer()
+        }.padding(.all)
+            .navigationTitle("Shazam Lyrics")
+        }
     }
 }
 
